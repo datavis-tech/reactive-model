541d629668c300f5e8c2ff08b7ae737754075b70
Curran <curran.kelleher@gmail.com>
2016-04-04 17:52:18 +0530

    Begin restructuring

---
+++ index.js
@@ -0,0 +1,126 @@
+// By Curran Kelleher April 2016
+
+// TODO change to proper modules.
+var ReactiveFunction = require("../reactive-function/index.js");
+var ReactiveProperty = require("../reactive-property/index.js");
+
+function ReactiveModel(){
+
+  // The model instance object.
+  // This is the value returned from the constructor.
+  var model = function (options){
+    console.log("Invoking model as a function");
+    //return Object.keys(options).map(function (outProperty){
+    //  var array = options[outProperty];
+    //  var callback = array.splice(array.length - 1)[0];
+    //  var inProperties = array;
+    //  return ReactiveFunction(inProperties, outProperty, callback);
+    //});
+    //ReactiveFunction.parse(options).forEach(function (λ){
+    //  assignNodes(λ);
+    //  addReactiveFunction(λ);
+    //});
+  };
+
+  // Keys are property names.
+  // Values are reactive properties.
+  var publicProperties = {};
+
+  // Keys are property names.
+  // Values are default values.
+  var defaultValues = {};
+
+  // Whether or not model.finalize() has been called.
+  // This is required to guarantee predictable behavior of setState and getState.
+  var isFinalized = false;
+
+  // Adds a public property to this model.
+  // The property name is required and will be used to reference this property.
+  // The default value is required to guarantee predictable behavior of setState and getState.
+  function addPublicProperty(propertyName, defaultValue, metadata){
+    // TODO test this
+    //if(isFinalized){
+    //  throw new Error("model.addPublicProperty() is being " +
+    //    "invoked after model.finalize, but this is not allowed. "+
+    //    "Public properties may only be added before the model is finalized.");
+    //}
+
+    // TODO test this
+    // if(!isDefined(defaultValue)){
+    //  throw new Error("model.addPublicProperty() is being " +
+    //    "invoked with an undefined default value. Default values for public properties " +
+    //    "must be defined, to guarantee predictable behavior. For public properties that " +
+    //    "are optional and should have the semantics of an undefined value, " +
+    //    "use ReactiveModel.NONE as the default value.");
+    //}
+
+    publicProperties[propertyName] = ReactiveProperty(defaultValue);
+    defaultValues[propertyName] = defaultValue;
+
+    return model;
+  }
+
+  function getDefaultValue(propertyName){
+    return defaultValues[propertyName];
+  }
+
+  function finalize(){
+    if(isFinalized){
+      throw new Error("model.finalize() is being invoked " +
+        "more than once, but this function should only be invoked once.");
+    }
+    isFinalized = true;
+
+    Object.keys(publicProperties).forEach(function(propertyName){
+      // TODO 
+      //track(propertyName);
+      //model[propertyName](getDefaultValue(propertyName));
+    });
+
+    return model;
+  }
+
+  function getState(){
+    //var state = {};
+    //Object.keys(publicProperties).forEach( function (publicProperty){
+    //  TODO omit default values.
+    //  state[publicProperty] = values[publicProperty];
+    //});
+    //return state;
+  }
+
+  function setState(state){
+
+    // TODO throw an error if some property in state
+    // is not in publicProperties
+    //Object.keys(state).forEach(function (property){
+    //  if(!property in publicProperties){
+    //    throw new Error("Attempting to set a property that has not" +
+    //      " been added as a public property in model.setState()");
+    //  }
+    //});
+
+    // Reset state to default values.
+    Object.keys(publicProperties).forEach(function (propertyName){
+      var defaultValue = publicProperties[propertyName];
+      model[propertyName](defaultValue);
+    });
+
+    // Apply values included in the new state.
+    Object.keys(state).forEach(function (propertyName){
+      var newValue = state[propertyName]
+      model[propertyName](newValue);
+    });
+
+    return model;
+  }
+
+  model.addPublicProperty = addPublicProperty;
+  model.finalize = finalize;
+  model.getState = getState;
+  model.setState = setState;
+
+  return model;
+}
+
+module.exports = ReactiveModel;
---
+++ test.js
@@ -0,0 +1,361 @@
+var ReactiveModel = require("./index");
+var assert = require("assert");
+
+function increment(x){
+  return x + 1;
+}
+
+function add(a, b){
+  return a + b;
+}
+
+describe("ReactiveModel", function (){
+
+  it("should be a function", function (){
+    assert.equal(typeof ReactiveModel, "function");
+  });
+
+  it("should add a public property and get state.", function (){
+    var model = new ReactiveModel();
+    model.addPublicProperty("x", 5);
+    //model.finalize();
+  });
+
+  //it("should throw an error if finalizing twice.", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.finalize();
+  //  assert.throws(model.finalize, Error);
+  //});
+
+  //it("should throw an error if adding public property after finalize", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.finalize();
+  //  assert.throws(model.addPublicProperty, Error);
+  //});
+
+  //it("should create getter-setters that get public properties", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.finalize();
+  //  assert.equal(model.x(), 5);
+  //});
+
+  //it("should create getter-setters that set public properties", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.finalize();
+  //  model.x(6);
+  //  assert.equal(model.x(), 6);
+  //});
+
+  //it("should create getter-setters that chain", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.addPublicProperty("y", 6);
+  //  model.finalize();
+  //  model.x(10).y(20);
+  //  assert.equal(model.x(), 10);
+  //  assert.equal(model.y(), 20);
+  //});
+
+  //it("should chain addPublicProperty and finalize", function (){
+  //  var model = new ReactiveModel()
+  //    .addPublicProperty("x", 5)
+  //    .addPublicProperty("y", 6)
+  //    .finalize();
+
+  //  model.y(20);
+
+  //  assert.equal(model.x(), 5);
+  //  assert.equal(model.y(), 20);
+  //});
+
+  //it("should get state", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.addPublicProperty("y", 10);
+  //  model.finalize();
+
+  //  var state = model.getState();
+  //  assert.equal(state.x, 5);
+  //  assert.equal(state.y, 10);
+  //});
+
+  //it("should get state after modification with getter-setters", function (){
+  //  var model = new ReactiveModel();
+  //  model.addPublicProperty("x", 5);
+  //  model.addPublicProperty("y", 10);
+  //  model.finalize();
+
+  //  model.x(10).y(20)
+
+  //  var state = model.getState();
+  //  assert.equal(state.x, 10);
+  //  assert.equal(state.y, 20);
+  //});
+  //
+  //function createModel(){
+  //  return new ReactiveModel()
+  //    .addPublicProperty("x", 5)
+  //    .addPublicProperty("y", 6)
+  //    .finalize();
+  //}
+
+  //it("should set state", function (){
+  //  var modelA = createModel().x(10).y(20);
+  //  var modelB = createModel();
+
+  //  modelB.setState(modelA.getState());
+
+  //  assert.equal(modelB.x(), 10);
+  //  assert.equal(modelB.y(), 20);
+  //});
+
+  //it("should set state that omits default values", function (){
+  //  var model = createModel().x(10).y(20);
+  //  model.setState({});
+  //  assert.equal(model.x(), 5);
+  //  assert.equal(model.y(), 6);
+  //});
+
+  //it("should chain setState", function (){
+
+  //  var model = createModel()
+  //    .x(10)
+  //    .y(20)
+  //    .setState({y: 45});
+
+  //  assert.equal(model.x(), 5);
+  //  assert.equal(model.y(), 45);
+  //});
+
+  //it("should react", function (){
+  //  var model = new ReactiveModel()
+  //    .addPublicProperty("a", 5)
+  //    .finalize();
+
+  //  model({
+  //    b: ["a", function (a){
+  //      return a + 1;
+  //    }]
+  //  });
+
+  //  ReactiveModel.digest();
+
+  //  assert.equal(model.b(), 6);
+  //});
+
+  //it("should react and use newly set value", function (){
+  //  var model = new ReactiveModel()
+  //    .addPublicProperty("a", 5)
+  //    .finalize();
+
+  //  model({
+  //    b: ["a", function (a){
+  //      return a + 1;
+  //    }]
+  //  });
+
+  //  model.a(7);
+  //  ReactiveModel.digest();
+  //  assert.equal(model.b(), 8);
+  //});
+
+  //it("should track when values change", function (){
+  //  var model = new ReactiveModel()
+  //    .addPublicProperty("a", 5)
+  //    .finalize();
+
+  //  model({
+  //    b: ["a", function (a){
+  //      return a + 1;
+  //    }]
+  //  });
+
+  //  model.a(7);
+  //  ReactiveModel.digest();
+  //  assert.equal(model.b(), 8);
+
+  //  model.a(8);
+  //  model.a(9);
+  //  model.a(10);
+  //  ReactiveModel.digest();
+  //  assert.equal(model.b(), 11);
+  //});
+
+  //it("should react with two input properties", function (){
+
+  //  var model = new ReactiveModel();
+
+  //  model({
+  //    fullName: ["firstName", "lastName", function (firstName, lastName){
+  //      return firstName + " " + lastName;
+  //    }]
+  //  });
+
+  //  model
+  //    .firstName("Jane")
+  //    .lastName("Smith");
+
+  //  ReactiveModel.digest();
+
+  //  assert.equal(model.fullName(), "Jane Smith");
+  //});
+
+  //it("should not react when only one of two input properties is defined", function (){
+  //  var model = new ReactiveModel();
+  //  var counter = 0;
+
+  //  model({
+  //    fullName: ["firstName", "lastName", function (firstName, lastName){
+  //      counter++;
+  //      return firstName + " " + lastName;
+  //    }]
+  //  });
+
+  //  model.firstName("Jane");
+
+  //  ReactiveModel.digest();
+
+  //  assert.equal(counter, 0);
+  //  assert.equal(model.fullName(), undefined);
+  //});
+
+  //it("should propagate two hops in a single digest", function (){
+
+  //  var model = new ReactiveModel();
+
+  //  model({
+  //    b: ["a", increment],
+  //    c: ["b", increment]
+  //  });
+
+  //  model.a(1);
+  //  ReactiveModel.digest();
+
+  //  assert.equal(model.a(), 1);
+  //  assert.equal(model.b(), 2);
+  //  assert.equal(model.c(), 3);
+  //});
+  //
+  //it("should evaluate digests independently", function (){
+  //  var model = new ReactiveModel();
+
+  //  var counterFullName = 0;
+  //  var counterB = 0;
+
+  //  model({
+  //    fullName: ["firstName", "lastName", function (firstName, lastName){
+  //      counterFullName++;
+  //      return firstName + " " + lastName;
+  //    }],
+  //    b: ["a", function (a){
+  //      counterB++;
+  //      return a + 1;
+  //    }]
+  //  });
+
+  //  model.firstName("John");
+  //  model.lastName("Doe");
+  //  ReactiveModel.digest();
+
+  //  assert.equal(counterFullName, 1);
+  //  assert.equal(counterB, 0);
+  //  assert.equal(model.fullName(), "John Doe");
+
+  //  model.a(1);
+  //  ReactiveModel.digest();
+
+  //  assert.equal(counterFullName, 1);
+  //  assert.equal(counterB, 1);
+  //  assert.equal(model.a(), 1);
+  //  assert.equal(model.b(), 2);
+  //  assert.equal(model.fullName(), "John Doe");
+  //});
+
+  //it("should evaluate tricky case", function (){
+
+  //  var model = new ReactiveModel();
+
+  //  // a - b
+  //  //       \
+  //  //        e
+  //  //       /
+  //  // c - d
+
+  //  model({
+  //    b: ["a", increment],
+  //    d: ["c", increment],
+  //    e: ["b", "d", add]
+  //  });
+
+  //  model.a(1).c(2);
+
+  //  ReactiveModel.digest();
+
+  //  assert.equal(model.e(), (1 + 1) + (2 + 1));
+  //});
+
+  //it("should auto-digest", function (done){
+  //  var model = new ReactiveModel()
+  //    .addPublicProperty("a", 5)
+  //    .finalize();
+
+  //  model({
+  //    b: ["a", function (a){
+  //      return a + 1;
+  //    }]
+  //  });
+
+  //  ReactiveModel.nextFrame(function(){
+  //    assert.equal(model.b(), 6);
+  //    model.a(10);
+  //    assert.equal(model.b(), 6);
+  //    ReactiveModel.nextFrame(function(){
+  //      assert.equal(model.b(), 11);
+  //      done();
+  //    });
+  //  });
+  //});
+
+  //it("should work with booleans", function (){
+  //  var model = new ReactiveModel();
+
+  //  model({
+  //    b: ["a", function (a){
+  //      return !a;
+  //    }]
+  //  });
+
+  //  model.a(false);
+  //  ReactiveModel.digest();
+  //  assert.equal(model.b(), true);
+
+  //  model.a(true);
+  //  ReactiveModel.digest();
+  //  assert.equal(model.b(), false);
+  //});
+
+  //it("should work with promises", function (done){
+  //  var model = new ReactiveModel();
+
+  //  model({
+  //    b: ["a", function (a){
+  //      return new Promise(function (resolve, reject){
+  //        setTimeout(function (){
+  //          resolve(a + 1);
+  //        }, 50);
+  //      });
+  //    }],
+  //    c: ["b", function (b){
+  //      assert.equal(b, 2);
+  //      done();
+  //    }]
+  //  });
+
+  //  model.a(1);
+
+  //});
+});
